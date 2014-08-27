7931c1d2cdcbaf87a4616e98d838de48f3d3b879
Marina Neumann <marinaneumann2468@yahoo.com>
2014-08-27 12:15:42 -0700

    Downcase the name of a desert before creating it.

--- lib/desert.rb
+++ lib/desert.rb
@@ -1,5 +1,14 @@
 class Desert < ActiveRecord::Base
 	validates :name, :presence => true
-	
+
 	has_many :meals
+
+	before_create :downcase_name
+
+	private
+
+	def downcase_name
+		self.name = self.name.downcase
+	end
+	
 end
--- spec/desert_spec.rb
+++ spec/desert_spec.rb
@@ -5,7 +5,13 @@ describe Desert do
 		new_desert = Desert.new({:name => ' '})
 		expect(new_desert.save).to eq false
 	end
-	
+
+	it 'should lowercase the name of a desert' do 
+		new_desert = Desert.create({:name => 'CHOCOLATE CHIP COOKIE'})
+		expect(new_desert.name).to eq 'chocolate chip cookie'
+	end
+
+
 	it 'has many meals' do 
 		test_desert = Desert.create({:name => 'Strawberry CheeseCake'})
 		test_meal = Meal.create(:desert_id => test_desert.id)
