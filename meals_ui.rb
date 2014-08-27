bbb508523e64267ef8eb96ac67ff24ad579554d4
Marina Neumann <marinaneumann2468@yahoo.com>
2014-08-27 15:38:32 -0700

    Add user ability to add an entree to the system.

--- meals_ui.rb
+++ meals_ui.rb
@@ -11,6 +11,7 @@ ActiveRecord::Base.establish_connection(development_configuration)
 def welcome
 	puts "Welcome to Meal Organizer!!!"
 	puts "Put 'e' to add an entree"
+	puts "Put 'l' to see menu"
 	puts "Put 'd' to add a dessert"
 	puts "Put 'r' to rate a dessert"
 	puts "Put 'm' to create a meal"
@@ -25,9 +26,20 @@ def welcome
 		rate_dessert
 	elsif choice == 'm'
 		create_meal
+	elsif choice == 'l'
+		view_menu
 	else 
 		puts "Validity is what you seek. Try again."
 	end
 end
+
+def add_entree
+	puts "What entree would you like to add?"
+	choice = gets.chomp
+	new_entree = Entree.create({:name => choice})
+	welcome
+end
+
+
 
 welcome
