0d02877618371a8661e41ca00365be3e82a87c5d
Marina Neumann <marinaneumann2468@yahoo.com>
2014-08-27 15:52:03 -0700

    Add ability to rate a dessert and create a meal in user interface.

--- meals_ui.rb
+++ meals_ui.rb
@@ -11,10 +11,11 @@ ActiveRecord::Base.establish_connection(development_configuration)
 def welcome
 	puts "Welcome to Meal Organizer!!!"
 	puts "Put 'e' to add an entree"
-	puts "Put 'l' to see menu"
+	puts "Put 'l' to see menu of food options"
 	puts "Put 'd' to add a dessert"
 	puts "Put 'r' to rate a dessert"
 	puts "Put 'm' to create a meal"
+	puts "Put 'v' to all created meal plans"
 
 	choice =gets.chomp
 
@@ -28,9 +29,13 @@ def welcome
 		create_meal
 	elsif choice == 'l'
 		view_menu
+	elsif choice == 'v'
+		view_meals
 	else 
 		puts "Validity is what you seek. Try again."
 	end
+
+
 end
 
 def add_entree
@@ -39,6 +44,44 @@ def add_entree
 	new_entree = Entree.create({:name => choice})
 	welcome
 end
+
+def add_dessert
+	puts "What dessert would you like to add?"
+	choice = gets.chomp
+	new_dessert = Dessert.create({:name => choice})
+	welcome
+end
+
+def rate_dessert
+	puts "Here are all your desserts:"
+	desserts = Dessert.all
+	desserts.each {|dessert| put dessert.name}
+	puts "Which dessert would you like to rate?"
+	choice = gets.chomp
+	dessert_choice = Dessert.where({:name => choice})
+	puts "What would you like to rate the dessert? 1 being completely awful to 5 which is epic."
+	rate_response = gets.chomp
+	rated_dessert = Dessert.create({:name => dessert_choice.name, :yum_factor => rate_response})
+	welcome
+end
+
+def create_meal
+	puts "Here are all your entrees:"
+	entrees = Entree.all
+	entrees.each {|entree| puts entree.name}
+	puts "Here are all your desserts:"
+	desserts = Dessert.all
+	desserts.each {|dessert| put dessert.name}
+	puts "Which entree would you like to select?"
+	entree_choice = gets.chomp
+	puts "Which dessert would you like to select?"
+	dessert_choice = gets.chomp
+	new_meal = Meal.create({:dessert_id => dessert_choice.id, :entree_id => entree_choice.id})
+end
+
+# def view_menu
+
+# end
 
 
 
