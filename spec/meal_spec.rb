946f0d56a12321742074250b5155a6508a2c9a9b
Marina Neumann <marinaneumann2468@yahoo.com>
2014-08-27 11:17:48 -0700

    Add association taht a meal will belong to a desert.

--- spec/meal_spec.rb
+++ spec/meal_spec.rb
@@ -6,4 +6,10 @@ describe Meal do
 		test_meal = Meal.create({:entree_id => test_entree.id})
 		expect(test_meal.entree).to eq test_entree
 	end
+
+	it 'belongs to a desert' do 
+		test_desert = Desert.create({:name => 'Slut Brownies'})
+		test_meal = Meal.create({:desert_id => test_desert.id})
+		expect(test_meal.desert).to eq test_desert
+	end
 end
